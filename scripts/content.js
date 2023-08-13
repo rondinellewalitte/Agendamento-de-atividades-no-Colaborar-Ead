862c55fce8d362a5d0ae3e9050eea7e325c07612
Rondinelle Walitte <rondinellewalitte@gmail.com>
2023-08-13 14:50:04 -0300

    new updates

--- scripts/content.js
+++ scripts/content.js
@@ -31,32 +31,78 @@ function start(){
     atividadesElements.forEach(element => {
       const classNameText = element.querySelector('.timeline-title span span:first-child').textContent.trim();
       const classNameDetailText = element.querySelector('.timeline-heading small').textContent.trim();
-      const textHifen = classNameDetailText.split('-')[1].trim();
-      const textFormat= textHifen.replace('-', ' ');
-      const classPeriodText = element.querySelector('.timeline-heading small:first-child').textContent.trim();
-      const classLinkVideo = element.querySelector('.row.form-group div a');
-      
-      if (classLinkVideo !== null) {
-        const classLinkVideoHref = classLinkVideo.getAttribute('href');
-        if (classLinkVideoHref !== null) {
-          const url = "www.colaboraread.com.br"+encodeSpecialCharacters(classLinkVideoHref);
+      if (classNameDetailText.includes('-')) {
+        const textHifen = classNameDetailText.split('-')[1].trim();
+        const textFormat= textHifen.replace('-', ' ');
+        const classPeriodText = element.querySelector('.timeline-heading small:first-child').textContent.trim();
+        const classLinkVideo = element.querySelector('.row.form-group div a');
+        
+        if (classLinkVideo !== null) {
+          const classLinkVideoHref = classLinkVideo.getAttribute('href');
+          if (classLinkVideoHref !== null) {
+            const url = "www.colaboraread.com.br"+encodeSpecialCharacters(classLinkVideoHref);
+            const timelineTitleElement = element.querySelector('.timeline-title');
+            const firstSpan = timelineTitleElement.querySelector('span:first-child');
+        
+            const newScheduleSpan = document.createElement('span');
+            newScheduleSpan.innerHTML = `
+              <a href="https://calendar.google.com/calendar/u/0/r/eventedit?text=${classNameText+" - "+textFormat}&dates=${formatarData(classPeriodText)}&details=%3Ch2%3ELink+para+aula%3C%2Fh2%3E%3Cp%3EAcessar+a+aula+abaixo%20%3Ca+href%3D%22https%3A%2F%2F${url}%22%3Eaqui%3C%2Fa%3E%3C%2Fp%3E
+              " class="btn btn-default float-right btn-atvd-msg-tutor js-bootstrap-tooltip" target="_blank" title="" data-original-title="Criar um evento do Google Agenda">
+                <i class="fa fa-calendar" aria-hidden="true"></i> <span>Agendar</span>
+              </a>
+            `;
+            firstSpan.appendChild(newScheduleSpan);
+          } else {
+            console.log('Video href attribute not found.');
+          }
+        } else {
+          console.log('Video link element not found.');
+        }
+      }else{
+        const classPeriodText = element.querySelector('.timeline-heading small:first-child').textContent.trim();
+        const classLinkVideo = element.querySelector('.row.form-group div a');
+        
+        if (classLinkVideo !== null) {
+          const classLinkVideoHref = classLinkVideo.getAttribute('href');
+          if (classLinkVideoHref !== null) {
+            const url = "www.colaboraread.com.br"+encodeSpecialCharacters(classLinkVideoHref);
+            const timelineTitleElement = element.querySelector('.timeline-title');
+            const firstSpan = timelineTitleElement.querySelector('span:first-child');
+        
+            const newScheduleSpan = document.createElement('span');
+            newScheduleSpan.innerHTML = `
+              <a href="https://calendar.google.com/calendar/u/0/r/eventedit?text=${classNameText+" - "+classNameDetailText}&dates=${formatarData(classPeriodText)}&details=%3Ch2%3ELink+para+aula%3C%2Fh2%3E%3Cp%3EAcessar+a+aula+abaixo%20%3Ca+href%3D%22https%3A%2F%2F${url}%22%3Eaqui%3C%2Fa%3E%3C%2Fp%3E
+              " class="btn btn-default float-right btn-atvd-msg-tutor js-bootstrap-tooltip" target="_blank" title="" data-original-title="Criar um evento do Google Agenda">
+                <i class="fa fa-calendar" aria-hidden="true"></i> <span>Agendar</span>
+              </a>
+            `;
+            firstSpan.appendChild(newScheduleSpan);
+          } else {
+            const timelineTitleElement = element.querySelector('.timeline-title');
+            const firstSpan = timelineTitleElement.querySelector('span:first-child');
+        
+            const newScheduleSpan = document.createElement('span');
+            newScheduleSpan.innerHTML = `
+              <a href="https://calendar.google.com/calendar/u/0/r/eventedit?text=${classNameText+" - "+classNameDetailText}&dates=${formatarData(classPeriodText)}" class="btn btn-default float-right btn-atvd-msg-tutor js-bootstrap-tooltip" target="_blank" title="" data-original-title="Criar um evento do Google Agenda">
+                <i class="fa fa-calendar" aria-hidden="true"></i> <span>Agendar</span>
+              </a>
+            `;
+            firstSpan.appendChild(newScheduleSpan);
+          }
+        } else {
           const timelineTitleElement = element.querySelector('.timeline-title');
           const firstSpan = timelineTitleElement.querySelector('span:first-child');
       
           const newScheduleSpan = document.createElement('span');
           newScheduleSpan.innerHTML = `
-            <a href="https://calendar.google.com/calendar/u/0/r/eventedit?text=${classNameText+" - "+textFormat}&dates=${formatarData(classPeriodText)}&details=%3Ch2%3ELink+para+aula%3C%2Fh2%3E%3Cp%3EAcessar+a+aula+abaixo%20%3Ca+href%3D%22https%3A%2F%2F${url}%22%3Eaqui%3C%2Fa%3E%3C%2Fp%3E
-            " class="btn btn-default float-right btn-atvd-msg-tutor js-bootstrap-tooltip" target="_blank" title="" data-original-title="Criar um evento do Google Agenda">
+            <a href="https://calendar.google.com/calendar/u/0/r/eventedit?text=${classNameText+" - "+classNameDetailText}&dates=${formatarData(classPeriodText)}" class="btn btn-default float-right btn-atvd-msg-tutor js-bootstrap-tooltip" target="_blank" title="" data-original-title="Criar um evento do Google Agenda">
               <i class="fa fa-calendar" aria-hidden="true"></i> <span>Agendar</span>
             </a>
           `;
           firstSpan.appendChild(newScheduleSpan);
-        } else {
-          console.log('Video href attribute not found.');
         }
-      } else {
-        console.log('Video link element not found.');
       }
+
     });
   }
 }
